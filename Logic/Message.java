c12a469b4087387be2bb5b8add6b650d018aaa6f
Vinícius Ventura <vini.ventura999@gmail.com>
2022-04-15 14:48:23 -0300

    Creating stubs for send messa feature.

--- Logic/Message.java
+++ Logic/Message.java
@@ -5,4 +5,17 @@ import java.util.LinkedList;
 
 public class Message extends Network {
     LinkedList <Account> mensagem = new LinkedList<>();
+    Scanner s = new Scanner(System.in);
+
+    public void sendMessage(String friendEmail, String message) {
+
+    }
+
+    public void replyMessage(String friendEmail, String message) {
+
+    }
+
+    public void showMessages(String email) {
+        
+    }
 }
