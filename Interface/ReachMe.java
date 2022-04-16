bbd75db34394d2597fb7228671df887cf4cf66dc
RaulMiguelAS <raul.2021104876@unicap.br>
2022-04-16 17:24:46 -0300

    project finished

--- Interface/ReachMe.java
+++ Interface/ReachMe.java
@@ -11,6 +11,7 @@ public class ReachMe {
     static Network account = new Network();
     static Friends friend = new Friends();
     static Message message = new Message();
+    
 
     public static void main(String[] args) {
         byte op = 1;
